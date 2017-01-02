9c5054c2010037d791e7690ce2fa1c9f5c4c0e98
Matt Fender <mjfender@gmail.com>
2017-01-02 10:46:44 -0500

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+  validates :name, length: { minimum: 1 }
+  validates :phone_number, length: { is: 10 }
+  validates :name, uniqueness: true
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { in: 50..500 }
+  validates :summary, length: { in: 1..100 }
+  validates :category, inclusion: { in: %w(Fiction Clickbait),
+    message: "%{value} is not a valid category" }
+  validates :title, exclusion: { in: ["True Facts"] }
+
+
 end
